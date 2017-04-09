d436ff246e5e0d20999a213c1b3bd1de2e108b67
Jack Lam <jacklam718@gmail.com>
2017-04-10 01:22:20 +0800

    init gh-pages

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import TabBar from './TabBar';
-import TabItem from './TabItem';
-import Tab from './Tab';
-import Tabs from './Tabs';
-
-export { TabBar, TabItem, Tab };
-export default Tabs;
--- stories/index.js
+++
@@ -1,3 +0,0 @@
-import './Tabs';
-import './Tab';
-import './TabBar';
